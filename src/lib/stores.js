9d06c4f67c920269dab4d15996f04cf56279a04f
tab9204 <tab9204@g.rit.edu>
2023-03-17 18:06:11 -0700

    fixed some things

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,8 +3,18 @@ import {writable} from 'svelte/store';
 
 export const pb = new PocketBase('https://www.chilionrap.com');
 
+console.log(pb.authStore.isValid);
+
+/*
+if(!pb.authStore.isValid){
+    pb.authStore.clear();
+}*/
+
 export const user = writable(pb.authStore.model);
+
 
 export const beginWorkout = writable(false);
 
 export const currentWorkoutId = writable(null);
+
+//export let allWorkouts = writable([]);
